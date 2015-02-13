15509fe45b533288c51f298457b77d6bc3976bdd
Grégory Bataille <gbataille.dev@gmail.com>
2015-02-13 15:30:44 +0100

    cleanup + haddock documentation

--- src/GB2/Vector.hs
+++ src/GB2/Vector.hs
@@ -4,8 +4,10 @@ module GB2.Vector
   vecAdd
 ) where
 
+-- | A 3D Vector
 data Vector = Vector Float Float Float 
             deriving (Eq, Show)
 
+-- | Adds 2 3D Vectors together
 vecAdd :: Vector -> Vector -> Vector
 vecAdd (Vector a b c) (Vector d e f) = Vector (a+d) (b+e) (c+f)
