1afc7fd7fd60325e030f366e9e5a062db423e606
Grégory Bataille <gbataille.dev@gmail.com>
2015-02-16 20:38:26 +0100

    Clamping of the PPM output

--- src/GB2/Color.hs
+++ src/GB2/Color.hs
@@ -18,7 +18,12 @@ type Image = (Integer, Integer, [Color]) -- Width, Height, Pixels
 
 -- Convert a floating point value color [0 1] ^ 3 to uint8 [0 255] ^ 3
 color2list :: Color -> [Integer]
-color2list (Vector a b c) = [truncate (a * 255), truncate (b * 255), truncate (c * 255)]
+color2list (Vector a b c) = [truncate2 (a * 255), truncate2 (b * 255), truncate2 (c * 255)]
+
+truncate2 a
+    | a <= 0 = 0
+    | a > 255 = 255
+    | otherwise = truncate a
 
 color2string :: Color -> String
 color2string color = unwords $ map show $ color2list color
